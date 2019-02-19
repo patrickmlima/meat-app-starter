8950e2b742a7e403ddaa5120de0956b18c11f805
Patrick Lima <patrickm.lima1@gmail.com>
2019-02-19 19:02:02 -0400

    Setup order-component to use ReactiveForms [frontend]

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { RadioOption } from '../shared/radio/radio-option.model';
@@ -12,6 +13,12 @@ import { OrderService } from './order.service';
 })
 export class OrderComponent implements OnInit {
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern = /^[0-9]*$/
+
+  orderForm: FormGroup;
+
   delivery: number = 8;
 
   paymentOptions: RadioOption[] = [
@@ -21,9 +28,31 @@ export class OrderComponent implements OnInit {
   ]
 
   constructor(private orderService: OrderService,
-    private router: Router) { }
+    private router: Router,
+    private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', Validators.required)
+    }, { validator: OrderComponent.equalsTo })
+  }
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+
+    if((email && emailConfirmation) &&
+      (email.value !== emailConfirmation.value)) {
+      return { emailsNotMatch: true }
+    }
+
+    return undefined;
   }
 
   itemsValue(): number {
