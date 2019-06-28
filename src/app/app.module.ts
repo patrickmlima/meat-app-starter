cee86e1162c208ac8d2c380e31123dbe95150d01
Patrick Lima <patrickm.lima1@gmail.com>
2019-06-28 16:33:41 -0400

    Implement user-detail component for login/logout and improve security methods [frontend]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './security/login/login.component';
-
+import { UserDetailComponent } from './header/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
@@ -39,7 +39,8 @@ import { LoginComponent } from './security/login/login.component';
     ReviewsComponent,
     OrderSummaryComponent,
     NotFoundComponent,
-    LoginComponent
+    LoginComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/app/security/login/login.service.ts
+++ src/app/security/login/login.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router'
+import { Router, NavigationEnd } from '@angular/router'
 
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/filter'
 
 import { MEAT_API } from '../../app.api'
 import { User } from './user.model'
@@ -13,12 +14,21 @@ export class LoginService {
 
     private user: User
 
-    constructor(private http: HttpClient, private router: Router) { }
+    private lastURL: string
+
+    constructor(private http: HttpClient, private router: Router) {
+        this.router.events.filter(e => e instanceof NavigationEnd)
+            .subscribe((e: NavigationEnd) => this.lastURL = e.url)
+    }
 
     login(email: string, password: string): Observable<User> {
         return this.http.post<User>(
             `${MEAT_API}/login`, { email: email, password: password })
             .do(user => this.user = user)
+    }
+
+    logout() {
+        this.user = undefined
     }
 
     isLoggedIn(): boolean {
@@ -29,7 +39,11 @@ export class LoginService {
         return this.isLoggedIn() ? this.user.accessToken : ""
     }
 
-    handleLogin(path?: string) {
+    handleLogin(path: string = this.lastURL) {
         this.router.navigate(['/login', btoa(path)])
+    }
+
+    getUser(): User {
+        return this.user
     }
 }
